70e28b883acf56caa77b6cbc5b560c93e3790736
Akshithvasu <150986280+Akshithvasu@users.noreply.github.com>
2026-01-21 12:08:18 +0530

    Updated project2.sh

--- project2.sh
+++ project2.sh
@@ -9,7 +9,7 @@ while read -r site; do
     code=$(curl -o /dev/null -s -w "%{http_code}" "http://$site")
     echo "$site -> HTTP $code"
 
-    if [[ "$code" -eq 301 ]]; then
+    if [[ "$code" -eq 200 ]]; then
         avg_latency=$(ping -c 5 "$site" | tail -1 | awk -F'/' '{print $5}')
         echo "Average latency to $site: ${avg_latency} ms"
     fi
