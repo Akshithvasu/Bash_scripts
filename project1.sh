a358148e3d4b66a29ea0617fe284c11659235db9
Akshithvasu <150986280+Akshithvasu@users.noreply.github.com>
2026-01-22 10:30:09 +0530

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-read -p "Threshold for CPU in %: " tc
-
 idle=$(top -bn1 | grep "Cpu(s)" | awk -F',' '{print $4}' | awk '{print $1}')
 cpu_used=$(echo "100 - $idle" | bc)
-if (( $(echo "$cpu_used > $tc" | bc -l) )); then
+if (( $(echo "$cpu_used > 50" | bc -l) )); then
     notify-send "Alert" "CPU usage is high: $cpu_used%"
 fi
-read -p "Threshold for Memory in %: " tm
 total=$(top -bn1 | grep "MiB Mem" | awk -F'[ :,]+' '{print $3}')
 used=$(top -bn1 | grep "MiB Mem" | awk -F'[ :,]+' '{print $7}')
 mem_used=$(echo "($used / $total) * 100" | bc -l)
-if (( $(echo "$mem_used > $tm" | bc -l) )); then
+if (( $(echo "$mem_used > 50" | bc -l) )); then
     notify-send "Alert" "Memory usage is high: $mem_used%"
 fi
 
-
-read -p "Threshold for Disk space in %: " td
 disk_used=$(df -h / | awk 'NR==2 {gsub("%",""); print $5}' )
-if (( disk_used > td )); then
+if (( disk_used > 80 )); then
     notify-send "Alert" "Disk usage is high: $disk_used%"
 fi
 
