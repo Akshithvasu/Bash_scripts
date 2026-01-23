be6cd9b164895e9a4c6e4aa3a248c5c29eb09070
Akshithvasu <150986280+Akshithvasu@users.noreply.github.com>
2026-01-23 10:00:25 +0530

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -6,13 +6,13 @@ ss -ln
 read -p "Enter the file : " file
 
 while read -r site; do
-    code=$(curl -o /dev/null -s -w "%{http_code}" "http://$site")
+    code=$(curl -o /dev/null -s -w "%{http_code}" "$site")
     echo "$site -> HTTP $code"
 
     if [[ "$code" -eq 200 ]]; then
-        avg_latency=$(ping -c 5 "$site" | tail -1 | awk -F'/' '{print $5}')
-        echo "Average latency to $site: ${avg_latency} ms"
+        host=$(echo "$site" | sed 's~https\?://~~; s~/.*~~')
+        avg_latency=$(ping -c 2 "$host" | tail -1 | awk -F'/' '{print $5}')
+        echo "Average latency to $host: ${avg_latency} ms"
     fi
 
 done < "$file"
-
